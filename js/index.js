393695536c8a821c4b216ac15b05798090d45573
benitomaxi <benitomaxi@gmail.com>
2024-08-28 22:42:09 -0300

    Tercera entrega

--- js/index.js
+++ js/index.js
@@ -1,57 +1,62 @@
-function cargaDeDatos(){
-    function autos (nombre, km, aceite, correa, cubierta) {
-             this.nombre = nombre;
-             this.km = km;
-             this.aceite= aceite;
-             this.correa= correa;
-             this.cubierta= cubierta;
-    
-    }
-      const autos1 = new autos (prompt("Ingrese la marca del auto"), prompt ("Ingrese sus km "), prompt ("Ingrese km de su ultimo cambio de aceite "), prompt ("Ingrese km de su ultimo cambio de correa") );
-      return autos1;
-    }
-    
-    function calcularCambioAceite(autos1){
-     
-      autos1.aceite=parseInt(autos1.aceite);
-      let cambioAceite = autos1.aceite+10000;
-     
-    
-      if (autos1.km - autos1.aceite > 10000) {
-        alert("¡Atención! El cambio de aceite está atrasado.");
-    } 
-            else {
-        console.log("el proximo cambio de aceite es en: " + cambioAceite + " km");
-    }
-    }
-    
-    function calcularCambioCorrea(autos1) {
-    
-        autos1.correa=parseInt(autos1.correa);
-        let cambioCorrea = autos1.correa+60000;
-    
-    if (autos1.km - autos1.correa > 60000) {
-        alert("¡Atención! El cambio de correa está atrasado.");
-    } 
-            else {
-        console.log("el proximo cambio de correa es en: " + cambioCorrea + " km");
-    }
-    }
-    
-    function mantenimiento (){
-    
-    
-        const autos1 = cargaDeDatos();
-    
-        console.log(autos1);
-    
-        calcularCambioAceite(autos1);
-        calcularCambioCorrea(autos1);
-    
-        const mantenimientoArray = [autos1.km, autos1.aceite + 10000, autos1.correa + 60000];
-        console.log(mantenimientoArray);
+   
+    const intervaloAceite = 10000; 
+    const intervaloCorrea = 60000; 
         
+document.getElementById('guardar').addEventListener('click', function() {
+    const marca = document.getElementById('marca').value;
+    const modelo = document.getElementById('modelo').value;
+    const km = document.getElementById('km').value;
+    const aceite = document.getElementById('aceite').value;
+    const correa = document.getElementById('correa').value;
+   
+    const auto = {
+        marca: marca,
+        modelo: modelo,
+        km: km,
+        aceite: aceite,
+        correa: correa
+    };
+    
+   
+    const autoJSON = JSON.stringify(auto);
+    
+    
+    localStorage.setItem('auto', autoJSON);
+    
+
+    const restanteAceite = intervaloAceite - (km - aceite);
+    const restanteCorrea = intervaloCorrea - (km - correa);
+
+    document.getElementById('mostrarMarca').innerHTML = `Marca: ${auto.marca}`;
+    document.getElementById('mostrarModelo').innerHTML = `Modelo: ${auto.modelo}`;
+    document.getElementById('mostrarkm').innerHTML = `km: ${auto.km}`;
+    document.getElementById('mostrarAceite').innerHTML = `aceite: ${auto.aceite}`;
+    document.getElementById('mostrarCorrea').innerHTML = `Correa: ${auto.correa}`;
+
+    
+    if (auto.km-auto.aceite < intervaloAceite) {
+        document.getElementById('cambioAceite').textContent = `Kilómetros restantes para el próximo cambio de aceite: ${restanteAceite} km`;
+    } else {
+        document.getElementById('cambioAceite').textContent = `¡Es hora de cambiar el aceite!`;
     }
+
+    console.log('Datos del auto:', auto);
+    console.log('Kilómetros restantes para el cambio de aceite:', restanteAceite);
+
+
+
+    if (auto.km-auto.correa < intervaloCorrea) {
+        document.getElementById('cambioCorrea').textContent = `Kilómetros restantes para el próximo cambio de correa: ${restanteCorrea} km`;
+    } else {
+        document.getElementById('cambioCorrea').textContent = `¡Es hora de cambiar la correa!`;
+    }
+
+    console.log('Datos del auto:', auto);
+    console.log('Kilómetros restantes para el cambio de correa dentada:', restanteCorrea);
+
+});
+
+   
     
-    mantenimiento();
-    
+
+  
